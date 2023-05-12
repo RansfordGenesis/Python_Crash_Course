bfc70522c3b771fbaad14a1d53dd1c836fe3bdcc
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-05-12 00:04:16 +0000

    Update inheritance

--- inheritance.py
+++ inheritance.py
@@ -17,15 +17,21 @@ class Car:
     def increment_odometer(self, miles):
         self.odometer += miles
         
+class Battery:
+    def __init__(self, battery=40):
+        self.battery = battery
+        
+    def describe_battery(self):
+        print(f"The battery life is {self.battery}.")
+        
 class ElectricCar(Car):
     
     def __init__(self, model, name, year):
         super().__init__(model, name, year)
-        self.battery = 40
+        self.battery = Battery()
         
-    def describe_battery(self):
-        return f"The battery life is {self.battery}."
+        
 
 future_car = ElectricCar('New', 'BMW', 2023)
 print(future_car.describe_it())
-print(future_car.describe_battery())
+future_car.battery.describe_battery()
